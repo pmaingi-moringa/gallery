4ee0975a0a5f5612a2727f85948583ae700c69f8
pmaingi-moringa <paul.maingi@student.moringaschool.com>
2024-09-23 17:25:58 +0300

    Confilicts fixes

---
+++ _config.js
@@ -0,0 +1,8 @@
+var config = {}
+// Update to have your correct username and password
+config.mongoURI = {
+    production: 'mongodb+srv://pmaingi:Muthyani1987@cluster0.nckkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    development: 'mongodb+srv://pmaingi:Muthyani1987@cluster0.nckkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    test: 'mongodb+srv://pmaingi:Muthyani1987@cluster0.nckkd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+}
+module.exports = config;
